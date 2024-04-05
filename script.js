b531707d8a2c869038e2dbbc6114c35253be2b1d
ayunimas <nimasayu322@gmail.com>
2024-04-05 15:22:01 +0700

    versi 1.3

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("Form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let jumlah = parseInt(document.getElementById("jumlah").value);
+    let bukuTerpilih = document.getElementById("buku").value;
+
+});
